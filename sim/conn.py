4ffbaa27043300a36b9c12cea197c82d32423076
Saurabh Pahwa <49368811+Kaiose31@users.noreply.github.com>
2024-04-03 15:55:52 -0700

    Revert "Dev"

--- sim/conn.py
+++ sim/conn.py
@@ -1,10 +1,10 @@
-from .rotor import Rotor
+import airsim
 import os
 
 SERVER_IP = os.getenv("AIRSIMHOST", "127.0.0.1")
 PORT = 41451
 
-client = Rotor(SERVER_IP, PORT, timeout_value=10)
+client = airsim.MultirotorClient(SERVER_IP, PORT, timeout_value=10)
 client.confirmConnection()
 client.enableApiControl(True)
 client.armDisarm(True)
