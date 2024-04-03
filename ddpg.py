50805087b3bb9c0f18c78326687030dba72337cb
Saurabh Pahwa <saurabhpahwa31@gmail.com>
2024-04-03 16:35:44 -0700

    disable replay buffer checkpoint to save space

--- ddpg.py
+++ ddpg.py
@@ -1,4 +1,3 @@
-from stable_baselines3.common.vec_env import DummyVecEnv, VecTransposeImage
 import torch
 from sim.drone_env import DroneEnv
 from stable_baselines3 import DDPG
@@ -13,17 +12,12 @@ IMG_SHAPE = (480, 640, 3)
 TARGET = [-30, -10, -20]
 env = DroneEnv(IMG_SHAPE, client, target=np.array(TARGET))
 
-
-# env = DummyVecEnv([lambda: Monitor(env)])
-
-# Save a checkpoint every 1000 steps
 checkpoint_callback = CheckpointCallback(
     save_freq=1000,
     save_path="model/ddpg",
     name_prefix="ddpg",
-    save_replay_buffer=True,
-    save_vecnormalize=True,
+    save_replay_buffer=False,
+    save_vecnormalize=False,
 )
-
 
 model = DDPG("CnnPolicy", env, tensorboard_log="data/DDPG_tensorboard", device=DEVICE, verbose=1).learn(total_timesteps=2000, callback=checkpoint_callback)
