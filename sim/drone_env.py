d3796336aab8bed46530b966a135c4ffa84b73c0
NathanKong06 <156229117+NathKong@users.noreply.github.com>
2024-04-08 13:29:47 -0700

    Reward function

--- sim/drone_env.py
+++ sim/drone_env.py
@@ -47,18 +47,71 @@ class DroneEnv(gymnasium.Env):
     def reset(self, seed=None, options=None):
         self._setup_flight()
         return self._get_obs(), {}
+    
+    def R_a(d_max, d_goal_t, d_goal_t_plus_1):
+        tanh_diff = np.tanh(d_max - d_goal_t)
+        
+        abs_tanh_diff = np.abs(tanh_diff)
+        
+        if d_goal_t - d_goal_t_plus_1 == 0 or abs_tanh_diff == 0:
+            return 0
+        else:
+            return abs_tanh_diff * ((d_goal_t - d_goal_t_plus_1) / np.abs(d_goal_t - d_goal_t_plus_1))
+        
+    def R_mz(D_cz, d_barrier_t, d_barrier_t_plus_1):
+        tanh_diff = np.tanh(D_cz - d_barrier_t)
+        
+        abs_tanh_diff = np.abs(tanh_diff)
+        
+        if d_barrier_t_plus_1 - d_barrier_t == 0 or abs_tanh_diff == 0:
+            return 0
+        else:
+            return abs_tanh_diff * ((d_barrier_t_plus_1 - d_barrier_t) / np.abs(d_barrier_t_plus_1 - d_barrier_t))
+        
+    def R_cz(D_cz, d_barrier_t, d_barrier_t_plus_1, d_max, d_goal_t, d_goal_t_plus_1):
+        tanh_diff_barrier = np.tanh(D_cz - d_barrier_t)
+        
+        abs_tanh_diff_barrier = np.abs(tanh_diff_barrier)
+        
+        if d_barrier_t_plus_1 - d_barrier_t == 0 or abs_tanh_diff_barrier == 0:
+            term_barrier = 0
+        else:
+            term_barrier = abs_tanh_diff_barrier * ((d_barrier_t_plus_1 - d_barrier_t) / np.abs(d_barrier_t_plus_1 - d_barrier_t))
+
+        tanh_diff_goal = np.tanh(d_max - d_goal_t)
+        
+        abs_tanh_diff_goal = np.abs(tanh_diff_goal)
+        
+        if d_goal_t - d_goal_t_plus_1 == 0 or abs_tanh_diff_goal == 0:
+            term_goal = 0
+        else:
+            term_goal = abs_tanh_diff_goal * ((d_goal_t - d_goal_t_plus_1) / np.abs(d_goal_t - d_goal_t_plus_1))
+        return term_barrier + term_goal
 
     def reward(self):
-        distance = self.state["dist_to_target"]
+        d_goal = self.state["dist_to_target"]
+        d_max = self.drone.d_max
+        d_cz = self.drone.d_cz
+        # d_mz = self.drone.d_mz
+        cz_points = self.drone.get_cz_points()
+
+        min_distance = -1
+        for _,point in enumerate(cz_points):
+            distance = np.linalg.norm(self.state.position.to_numpy_array() - point)
+            if distance < min_distance:
+                min_distance = distance
+        d_barrier = min_distance
 
         if self.drone.simGetCollisionInfo().has_collided:
-            reward = -100
-        elif distance <= 10:
-            reward = 1
-        else:
-
-            reward = -self.state["dist_to_target"]
-        done = False
-        if reward <= -100:
             done = True
+            reward = -1000
+        elif (self.drone.points_mz() == 0 and self.drone.points_cz() == 0):
+            reward = self.R_a(d_max,d_goal,1)
+            done = False
+        elif (self.drone.points_cz() > 0 and self.drone.points_mz() == 0):
+            reward = self.R_cz(d_cz,d_barrier,1,d_max,d_goal,1)
+            done = False
+        elif (self.drone.points_mz() > 0):
+            reward = self.R_mz(d_cz,d_barrier,1)
+            done = False
         return reward, done
